2d9d33dacfcb7ec2aa413e46e28ee5f66f28914d
THIET <anhthiet92@anhthiet92buh@gmail.com>
2020-10-28 23:14:40 +0700

    main

--- Test/testTime.c
+++ Test/testTime.c
@@ -4,8 +4,11 @@
 int main(int argc, char const *argv[])
 {
     struct tm *timer_settime;
+    time_t timehientai;
+    time(&timehientai);
     
-    
+    timer_settime = gmtime(&timehientai);
+    printf("Thoi gian hien tai: %d",timer_settime);
 
 
 
