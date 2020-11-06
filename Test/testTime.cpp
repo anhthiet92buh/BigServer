7b3c36d9a4a0298ec5a243371ba4d15938512996
THIET <anhthiet92@anhthiet92buh@gmail.com>
2020-11-06 21:24:44 +0700

    main

--- Test/testTime.cpp
+++ Test/testTime.cpp
@@ -8,7 +8,7 @@ int main(int argc, char const *argv[])
     time(&timehientai);
     
     timer_settime = gmtime(&timehientai);
-    printf("Thoi gian hien tai: %s",timer_settime->tm_isdst);
+    printf("Thoi gian hien tai: %d",timer_settime->tm_isdst);
 
 
 
