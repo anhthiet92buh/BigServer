004a8d87ddce807cfe0f1a87d448b6c2e452e78b
THIET <anhthiet92@anhthiet92buh@gmail.com>
2020-11-08 23:18:35 +0700

    main

--- tuioxyz.h
+++ tuioxyz.h
@@ -2,7 +2,20 @@
 #include<bitset>
 #include<stdio.h>
 #include <time.h>
-struct tuioxyz
+#include <string.h>
+#include "FullMacro.h"
+struct tuioxyz //bao gom Time: t, ID User: U, ID: i, toa do: oxyz
 {
+    time_t t;
+    double user;
+    long double Id;
+    Oxyz oxyz;
     
+    
+};
+struct Oxyz
+{
+    double x;
+    double y;
+    double z;
 };
