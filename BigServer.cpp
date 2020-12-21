7d65123afebc7ded43da3314c67d04b352ecd356
anhthiet92buh <43349109+anhthiet92buh@users.noreply.github.com>
2020-12-22 00:57:47 +0700

    main

--- BigServer.cpp
+++ BigServer.cpp
@@ -11,27 +11,34 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     vector<Tuioxyz> all_point_vn;
-    Tuioxyz point_M, Pteamplate[100];
+    Tuioxyz point_M, Pteamplate;
 
-    ofstream oxy,f;
-    oxy.open("oxyz.vn",ios::app|ios::in);
-
-    //oxy.getloc()
-
-    cout<<"Time: "<<point_M.t<<" IdUser: "<<point_M.user<<" IDValue: "<<point_M.Id<<" Toa do: ("<<point_M.oxyz.x<<","<<point_M.oxyz.y<<","<<point_M.oxyz.z<<")"<<endl;
-    cout<<&point_M<<" - "<<endl;
+    int it;
+    fstream oxy,f;
+    oxy.open("oxyz.vn");
+    // cout<<"Time: "<<point_M.t<<" IdUser: "<<point_M.user<<" IDValue: "<<point_M.Id<<" Toa do: ("<<point_M.oxyz.x<<","<<point_M.oxyz.y<<","<<point_M.oxyz.z<<")"<<endl;
+    // cout<<&point_M<<" - "<<endl;
     for (size_t i = 0; i < 100; i++)
     {
-        //cout<<i<<endl;
         point_M.Id=i;
-        all_point_vn.push_back(point_M);
-        // InFileOxyz(&all_point_vn[i],&oxy);
+        all_point_vn.push_back(point_M);  
+        InFileOxyz(&point_M,&oxy);      
     };
+
     oxy.close();
+    f.open("oxyz.vn");
+    
+    while (!f.eof())
+    {
+        f.read((char*)&it,sizeof(int));
+        cout<<it<<endl;
+    };
+    
+
     // f.open("oxyz.vn");
     // cout<<sizeof(f);
-    // f.close();    
-
+    f.close();    
+    system("pause");
 
     return 0;
 }
--- save.h
+++ save.h
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-void InFileOxyz(Tuioxyz *t,ofstream *oxy){
-    *oxy<<*t<<endl;
+void InFileOxyz(Tuioxyz *t,fstream *oxy){
+    //oxy->open("oxyz.vn",ios::app);
+    oxy->write((char*) t,sizeof(t));
     };
+
+void ReadFileOxyz(Tuioxyz *t, fstream *oxy){
+
+    oxy->read((char*)t,sizeof(t));
+};
+
+void InTuioxyz(Tuioxyz *t){
+    cout<<t->t<<"   "<<t->user<<"   "<<t->Id<<" ("<<t->oxyz.x<<","<<t->oxyz.y<<","<<t->oxyz.z<<")"<<endl;
+
+
+
+
+
+};
 
 
 
