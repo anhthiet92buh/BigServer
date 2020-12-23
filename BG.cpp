64629f174bf358b790330ec8a983e5f2029a2ad8
anhthiet92buh <43349109+anhthiet92buh@users.noreply.github.com>
2020-12-23 23:38:38 +0700

    main

---
+++ BG.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include "BigFunction.h"
+#include "tuioxyz_struct.h"
+#include "save.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    
+    
+    
+    
+    
+    return 0;
+}
--- tuioxyz_struct.h
+++ tuioxyz_struct.h
@@ -2,14 +2,14 @@
 #include <time.h>
 //bao gom Time: t, ID User: U, ID: i, toa do: oxyz
 typedef struct{
-    int64_t x;
-    int64_t y;
-    int64_t z;
+    int64_t x=0;
+    int64_t y=0;
+    int64_t z=0;
 }Oxyz;
 
 typedef struct{
-    time_t t;
-    double user;
-    long double Id;
+    time_t t=0;
+    double user=0;
+    long double Id=0;
     Oxyz oxyz;       
 }Tuioxyz;
