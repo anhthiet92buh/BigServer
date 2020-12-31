8a79ade0f4f37cdd7972bf7eb8c028f869eda43c
anhthiet92buh <43349109+anhthiet92buh@users.noreply.github.com>
2020-12-31 19:19:14 +0700

    main

--- ThuVienVN/tuioxyz_struct.h
+++ ThuVienVN/tuioxyz_struct.h
@@ -1,5 +1,13 @@
-#include<iostream>
-#include <time.h>
+#ifndef TUIOXYZ_STRUCT_H
+#define TUIOXYZ_STRUCT_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include    <iostream>
+#include    <time.h>
 //bao gom Time: t, ID User: U, ID: i, toa do: oxyz
 typedef struct{
     int64_t x=0;
@@ -7,15 +15,30 @@ typedef struct{
     int64_t z=0;
 }Oxyz;
 
-typedef struct user
-{
-    /* data */
-};
-
-
 typedef struct{
     time_t t=0;
     double user=0;
     long double Id=0;
     Oxyz oxyz;       
 }Tuioxyz;
+
+typedef struct
+{
+    long double allPoint=0;
+    Tuioxyz ValuePoint;
+
+}PointVN;
+
+typedef struct {
+    long double len=0;
+    Tuioxyz 
+
+
+}lens;
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
