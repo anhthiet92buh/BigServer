2a30904882cb44c904d417d6140cee2d16604ce8
anhthiet92buh <43349109+anhthiet92buh@users.noreply.github.com>
2021-01-03 22:18:41 +0700

    main

--- ThuVienVN/tuioxyz_struct.h
+++ ThuVienVN/tuioxyz_struct.h
@@ -9,30 +9,36 @@ extern "C"
 #include    <iostream>
 #include    <time.h>
 //bao gom Time: t, ID User: U, ID: i, toa do: oxyz
-typedef struct Oxyz{
+typedef struct Oxyz {
     int64_t x=0;
     int64_t y=0;
     int64_t z=0;
 };
 
-typedef struct Tuioxyz{
+typedef struct Tuioxyz  {
     time_t t=0;
     double user=0;
     long double Id=0;
     Oxyz oxyz;       
 };
 
-typedef struct PointVN{
+typedef struct PointVN  {
     long double allPoint=0;
     Tuioxyz ValuePoint;
 
 };
 
 
-typedef struct lens{
+typedef struct len  {
     long double len=0;
-    PointVN
+    PointVN onePointVN;
 };
+
+typedef struct lenOfPointVN {
+    PointVN PointVN_Goc;
+    long double lens[];
+};
+
 
 #ifdef __cplusplus
 }
