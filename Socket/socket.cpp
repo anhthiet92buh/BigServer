fc111cd21be3878a666d965709983ea135ab054e
anhthiet92buh <43349109+anhthiet92buh@users.noreply.github.com>
2021-01-19 10:20:36 +0700

    main

--- Socket/socket.cpp
+++ Socket/socket.cpp
@@ -3,3 +3,16 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <time.h>
+
+int main(int argc, char const *argv[])
+{
+    int sockfd = -1;
+    struct sockaddr_in 
+
+
+
+
+
+
+    return 0;
+}
