ef4b8609e809a042235a7b2838b0860b5926919f
Thiet <anhthiet92buh@gmail.com>
2021-01-19 16:25:30 +0700

    main

--- Socket/client.cpp
+++ Socket/client.cpp
@@ -3,6 +3,11 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <time.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
@@ -14,8 +19,20 @@ int main(int argc, char const *argv[])
     memset(recv_buffer,0,sizeof(recv_buffer));  //set lai gia tri
     memset(&server_addr,0,sizeof(server_addr));
 
-    sockfd = socket(AF_INET, SOCKS_STREAM,0);
-    server_addr.sin_
+    sockfd = socket(AF_INET, SOCK_STREAM,0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(5000);
+
+    if (connect(sockfd,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
+    {
+        read(sockfd, recv_buffer,sizeof(recv_buffer)-1);
+        printf("\n%s\n",recv_buffer);        
+        close(sockfd);
+    };
+    
+
+
 
 
 
---
+++ Socket/server.cpp
@@ -0,0 +1,46 @@
+#include <sys/socket.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <ctime>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int listenfd = -1;
+    int connfd = -1;
+    struct sockaddr_in server_addr;
+    char send_buffer[1024];
+    time_t ticks;
+
+    memset(send_buffer,0,sizeof(send_buffer));  //set lai gia tri
+    memset(&server_addr,0,sizeof(server_addr));
+
+    listenfd = socket(AF_INET, SOCK_STREAM,0);
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_addr.sin_port = htons(5000);
+
+    bind(listenfd, (struct sockaddr *)&server_addr,sizeof(server_addr));
+    listen(listenfd,100);//Lang nghe 100 socket client
+
+    while (1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        std::cout<<"Vua khoi tao ket noi Client den Server";
+        ticks = time(NULL);
+        sprintf(send_buffer, "Server phan hoi luc %s", ctime(&ticks));
+        write(connfd, send_buffer, strlen(send_buffer));
+        close(connfd);
+    };
+    
+
+
+    return 0;
+}
