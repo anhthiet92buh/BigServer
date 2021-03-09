24e18d1feccec69e1fb5cfc68665046a06d738d8
Nguyen Anh Thiet <anhthiet92buh@gmail.com>
2021-03-09 23:35:13 +0700

    main

--- backend/BLmain.cpp
+++ backend/BLmain.cpp
@@ -13,7 +13,7 @@
 #include <ctime>
 #include <wchar.h>
 //...........
-#define __STDC_FORMAT_MACROS 1
+// #define __STDC_FORMAT_MACROS 1
 #include <inttypes.h>
 
 // #include <stdio.h>
@@ -28,8 +28,6 @@
 
 
 using namespace std;
-using namespace icu;
-
 
 #ifndef UPRV_LENGTHOF
 #define UPRV_LENGTHOF(array) (int32_t)(sizeof(array)/sizeof((array)[0]))
@@ -49,6 +47,9 @@ const char *conninfo = "host=postgres hostaddr=127.0.0.1 port=7532 user=postgres
 static PGconn *conn;
 static PGresult *res;
 
+//Unicode
+static UConverter *cnv=NULL;
+
 
 
 //..................
@@ -57,6 +58,80 @@ static void exit_nicely(PGconn *conn){
     //exit(1);
 };
 
+//Ham cua unicode
+static void printUString(const char *announce, const UChar *s, int32_t length) {
+    static char out[200];
+    UChar32 c;
+    int32_t i;
+    UErrorCode errorCode=U_ZERO_ERROR;
+
+    /*
+     * Convert to the "platform encoding". See notes in printUnicodeString().
+     * ucnv_fromUChars(), like most ICU APIs understands length==-1
+     * to mean that the string is NUL-terminated.
+     */
+    ucnv_fromUChars(cnv, out, sizeof(out), s, length, &errorCode);
+    if(U_FAILURE(errorCode) || errorCode==U_STRING_NOT_TERMINATED_WARNING) {
+        printf("%sproblem converting string from Unicode: %s\n", announce, u_errorName(errorCode));
+        return;
+    }
+
+    printf("%s%s {", announce, out);
+
+    /* output the code points (not code units) */
+    if(length>=0) {
+        /* s is not NUL-terminated */
+        for(i=0; i<length; /* U16_NEXT post-increments */) {
+            U16_NEXT(s, i, length, c);
+            printf(" %04x", c);
+        }
+    } else {
+        /* s is NUL-terminated */
+        for(i=0; /* condition in loop body */; /* U16_NEXT post-increments */) {
+            U16_NEXT(s, i, length, c);
+            if(c==0) {
+                break;
+            }
+            printf(" %04x", c);
+        }
+    }
+    printf(" }\n");
+};
+
+static void demo_C_Unicode_strings() {
+    printf("\n* demo_C_Unicode_strings() --------- ***\n\n");
+
+    static const UChar text[]={ 0x41, 0x42, 0x43, 0 };          /* "ABC" */
+    static const UChar appendText[]={ 0x61, 0x62, 0x63, 0 };    /* "abc" */
+    static const UChar cmpText[]={ 0x61, 0x53, 0x73, 0x43, 0 }; /* "aSsC" */
+    UChar buffer[32];
+    int32_t compare;
+    int32_t length=u_strlen(text); /* length=3 */
+
+    /* simple ANSI C-style functions */
+    buffer[0]=0;                    /* empty, NUL-terminated string */
+    u_strncat(buffer, text, 1);     /* append just n=1 character ('A') */
+    u_strcat(buffer, appendText);   /* buffer=="Aabc" */
+    length=u_strlen(buffer);        /* length=4 */
+    printUString("should be \"Aabc\": ", buffer, -1);
+
+    /* bitwise comparing buffer with text */
+    compare=u_strcmp(buffer, text);
+    if(compare<=0) {
+        printf("String comparison error, expected \"Aabc\" > \"ABC\"\n");
+    }
+
+    /* Build "A<sharp s>C" in the buffer... */
+    u_strcpy(buffer, text);
+    buffer[1]=0xdf; /* sharp s, case-compares equal to "ss" */
+    printUString("should be \"A<sharp s>C\": ", buffer, -1);
+
+    /* Compare two strings case-insensitively using full case folding */
+    compare=u_strcasecmp(buffer, cmpText, U_FOLD_CASE_DEFAULT);
+    if(compare!=0) {
+        printf("String case insensitive comparison error, expected \"AbC\" to be equal to \"ABC\"\n");
+    }
+}
 
 //...................
 int main(int argc, char const *argv[])
@@ -75,6 +150,24 @@ int main(int argc, char const *argv[])
     time_t ticks;
     int nFields;
     char query[5000];
+
+    //Unicode
+    UErrorCode errorCode=U_ZERO_ERROR;
+    //Close unicode
+
+    cnv=ucnv_open(NULL, &errorCode);
+    if(U_FAILURE(errorCode)) {
+        fprintf(stderr, "error %s opening the default converter\n", u_errorName(errorCode));
+        return errorCode;
+    }
+
+    ucnv_setFromUCallBack(cnv, UCNV_FROM_U_CALLBACK_ESCAPE, UCNV_ESCAPE_C, NULL, NULL, &errorCode);
+    if(U_FAILURE(errorCode)) {
+        fprintf(stderr, "error %s setting the escape callback in the default converter\n", u_errorName(errorCode));
+        ucnv_close(cnv);
+        return errorCode;
+    }
+    //Close unicode
 
 
     listenfd = socket(AF_INET, SOCK_STREAM,0); //Sever tao socket chuyen nhan(listen) cac ket noi tu client
@@ -166,16 +259,23 @@ int main(int argc, char const *argv[])
         };
 
 
-        printf("Kiem tra byte tren du lieu: \n");
-        char xchar[100];
-        UChar uchar_test[100];
-        // xchar = *PQgetvalue(res,0,1);
-        u_charsToUChars(PQgetvalue(res,0,1),uchar_test,sizeof(*PQgetvalue(res,0,1))); /* include the terminating NUL */
-        u_UCharsToChars(uchar_test,xchar,sizeof(uchar_test));
-        printf("Byte cua xchar: %s      -sizeof(xchar): ",xchar); std::cout<<sizeof(xchar)<<endl;
-        printf("Byte cua int PQgetvalue(res,0,1): "); std::cout<<sizeof(PQgetvalue(res,0,1))<<endl;
+        // printf("Kiem tra byte tren du lieu: \n");
+        wchar_t xchar[100];
+        int it;
+        // UChar uchar_test[100];
+        std::mbtowc(xchar,PQgetvalue(res,0,1),1);
+        std::wcout << L"In PQgetvalue(res,0,1) theo Wchar: "<<xchar<<endl;
+        // printf("Kiem dinh: %d\n",it);
+        // u_charsToUChars(PQgetvalue(res,0,1),uchar_test,100); /* include the terminating NUL */
+        // u_UCharsToChars(uchar_test,xchar,100);
+        // printf("Byte cua xchar: %s      -sizeof(xchar): ",xchar);
+        // std::cout<<sizeof(xchar)<<endl;
+        // printf("Byte cua int PQgetvalue(res,0,1): "); std::cout<<sizeof(PQgetvalue(res,0,1))<<endl;
+        std::cout<<"Kiem tra *PQgetvalue: "<<endl;
+        // printf("---------%s--------",(char16_t)*PQgetvalue(res,0,1));
+        std::cout<<*PQgetvalue(res,0,1)<<endl;
 
-        printf("in xchar: ");
+        printf("in xchar: \n");
         // printf("%X\n",xchar);
         PQclear(res);
 
@@ -186,6 +286,7 @@ int main(int argc, char const *argv[])
         sleep(1);
                 
         close(connfd);
+        // demo_C_Unicode_strings();
         
     };
     
