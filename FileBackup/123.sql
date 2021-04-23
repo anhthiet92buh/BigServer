9f71d374182583cba31344fae53fe3abc52b96f3
Thiet <anhthiet92buh@gmail.com>
2021-04-23 08:44:17 +0700

    main

--- FileBackup/123.sql
+++ FileBackup/123.sql
@@ -1,12 +1,12 @@
 do $$
 DECLARE var1 integer;
 DECLARE var2 integer;
+DECLARE var integer:=0;
 begin
-var:=0;
 for i in 33..127 loop
 for j in 33..127 loop
-INSERT INTO public.two_char_c(x, y, z, t, two_char_c) VALUES (2,0,var,timeofday(),CONCAT(chr(i),chr(j))
-var := var+1;
+INSERT INTO public.two_char_c(x, y, z, t, two_char_c) VALUES (2,0,var,timeofday(),CONCAT(chr(i),chr(j)));
+var := var + 1;
 end loop;
 end loop;
 end;
